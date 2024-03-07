93bf4eebf901427325fda7dac992fb0b2f38c49f
Tom Coupe <thomasrcoupe97@gmail.com>
2024-03-07 18:12:17 +0100

    chore: add routes

--- src/pages/SpotifyAppSecrets.tsx
+++ src/pages/SpotifyAppSecrets.tsx
@@ -1,16 +1,14 @@
-import { useSpotifyAccessToken } from "./spotify/useSpotifyAccessToken";
+import { useSpotifyAccessToken } from "../spotify/useSpotifyAccessToken";
 
-const App = () => {
+export const SpotifyAppSecrets = () => {
   const { accessToken, status } = useSpotifyAccessToken();
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">spotql</h1>
+      <h1 className="text-3xl font-bold underline">SpotQL</h1>
       <p>Client ID: {import.meta.env.VITE_SPOTIFY_CLIENT_ID}</p>
       <p>Client Secret: {import.meta.env.VITE_SPOTIFY_CLIENT_SECRET}</p>
       <p>Access Token: {accessToken ?? status}</p>
     </>
   );
 };
-
-export default App;
