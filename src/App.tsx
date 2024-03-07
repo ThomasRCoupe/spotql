a490cf5bd21a79aee995c7b6ff538c364aaf2b6e
Tom Coupe <thomasrcoupe97@gmail.com>
2024-03-07 07:14:09 +0100

    chore: add access token request

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-function App() {
+import { useEffect, useState } from "react";
+import { getAccessToken } from "./spotify/auth";
+
+const App = () => {
+  const [accessToken, setAccessToken] = useState<string>();
+
+  useEffect(() => {
+    const getAndSetAccessToken = async () => {
+      const response = await getAccessToken();
+      if (!response.ok) {
+        return;
+      }
+
+      const token = await response.json();
+      setAccessToken(token.access_token);
+    };
+
+    void getAndSetAccessToken();
+  });
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">spotql</h1>
+      <p>Client ID: {import.meta.env.VITE_SPOTIFY_CLIENT_ID}</p>
+      <p>Client Secret: {import.meta.env.VITE_SPOTIFY_CLIENT_SECRET}</p>
+      <p>Access Token: {accessToken}</p>
     </>
   );
-}
+};
 
 export default App;
